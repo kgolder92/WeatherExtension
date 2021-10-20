3db89e0a06be5406b602dcb78324fd2c8537eb45
kgolder92 <golderkayla@gmail.com>
2021-10-20 13:04:45 -0400

    add segue to go back to the main view when cancel button is clicked

--- WeatherExtension/Controller/ViewController.swift
+++ WeatherExtension/Controller/ViewController.swift
@@ -9,22 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cityLable: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var countryLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     
+    var city = "San Francisco"
+    var country = "U.S"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        weatherLabel.text = ""
+        temperatureLabel.text = ""
     }
 
     @IBAction func updateWeatherInfo(_ sender: UIButton) {
 
         performSegue(withIdentifier: "showLocations", sender: self)
+        
     }
     
+ 
+    
+    
+    
+    //MARK: - Navigation
+    
+    @IBAction func unwindToHome(_ segue: UIStoryboardSegue) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showLocations" {
+            let destinationController = segue.destination as! UINavigationController
+            let locationTableViewController = destinationController.viewControllers[0] as! LocationTableViewController
+            locationTableViewController.selectedLocation = "\(city), \(country)"
+        }
+    }
     
 }
 
